57c6397c9beaab35f80275f627ed6392b2484c80
yskzfjt <fujita@tacit-knowledge-lab.com>
2015-10-10 21:50:59 +0900

    added CubeScript.cs
    greeting test script

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	Debug.Log("Hello World");
+	
+	}
+}
